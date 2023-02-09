199c89362a53731a5c2ddc82c504cd7aa8dbafcb
GooDongYeon <121007224+GooDongYeon@users.noreply.github.com>
2023-02-09 19:04:12 +0900

    java

---
+++ CodingTest Lv1/CodingTestJava/src/CodingTest/삼총사.java
@@ -0,0 +1,16 @@
+package CodingTest;
+
+public class 삼총사 {
+  public static void main(String[] args) {
+    Solution2 sol2 = new Solution2();
+    System.out.println(sol2);
+  }
+}
+
+class Solution2 {
+  public int solution(int[] number) {
+    int answer = 0;
+
+    return answer;
+  }
+}
