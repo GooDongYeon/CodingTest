fcce23df2fa5f4853e1c033c20d920c9f8140c26
GooDongYeon <121007224+GooDongYeon@users.noreply.github.com>
2023-02-09 22:34:51 +0900

    java

--- CodingTest Lv1/CodingTestJava/src/CodingTest/삼총사.java
+++ CodingTest Lv1/CodingTestJava/src/CodingTest/삼총사.java
@@ -1,9 +1,11 @@
 package CodingTest;
 
+
 public class 삼총사 {
   public static void main(String[] args) {
-    Solution2 sol2 = new Solution2();
-    System.out.println(sol2);
+    Solution2 sol = new Solution2();
+    int[] threeMan = {-2, 3, 0, 2, -5};
+    System.out.println(sol.solution(threeMan));
   }
 }
 
@@ -11,6 +13,15 @@ class Solution2 {
   public int solution(int[] number) {
     int answer = 0;
 
+    for (int i = 0; i < number.length; i++) {
+      for (int j = i + 1; j < number.length; j++) {
+        for (int j2 = j + 1; j2 < number.length; j2++) {
+          if((number[i]) + number[j] + number[j2] == 0) {
+            answer += 1;
+          }
+        }
+      }
+    }
     return answer;
   }
 }
